de1b9601a4250ae6d73d561e7446bee49ca7ae55
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2023-05-03 10:45:53 -0500

    pdated landing page, urls  to view services and commands

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import json
 from typing import Annotated
-from configs.services import Commands, Services
+from configs.services import services
+from configs.commands import commands
 
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -16,18 +17,18 @@ def index():
 
 @app.get("/service/{service_name}")
 def get_service(service_name: str):
-    return {"Service": Services[service_name]}
+    return {"Service": services[service_name]}
 
 
 @app.get("/service/{service_name}/components")
 def get_service_components(service_name: str):
-    return {"Components": Services[service_name]}
+    return {"Components": services[service_name]}
 
 
 @app.get("/service/{service_name}/commands")
 def get_service_commands(service_name: str):
     commands = [
-        component["commands"] for component in Services[service_name]["components"] 
+        component["commands"] for component in services[service_name]["components"] 
         if component.get("commands", None) != None
     ]
     return {f"Commands for {service_name}": commands}
@@ -35,11 +36,11 @@ def get_service_commands(service_name: str):
 
 @app.get("/commands")
 def get_commands():
-    return {"Commands": Commands}
+    return {"Commands": commands}
 
 
 @app.get("/commands/{command}")
 def get_command(command: str):
-    commands = {cmd["name"]: cmd for cmd in Commands}
+    commands = {cmd["name"]: cmd for cmd in commands}
     return {"Command": commands[command]}
 
