2315558f942f72b8942ccbdcec34e8c5419a4672
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2023-05-03 10:40:22 -0500

    Updated landing page, urls to view services and commands

--- src/main.py
+++ src/main.py
@@ -1,15 +1,45 @@
-from typing import Union
+import json
+from typing import Annotated
+from configs.services import Commands, Services
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index():
+    return {"Service Manager API": "Navigate to /docs or /redoc for API spec"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/service/{service_name}")
+def get_service(service_name: str):
+    return {"Service": Services[service_name]}
+
+
+@app.get("/service/{service_name}/components")
+def get_service_components(service_name: str):
+    return {"Components": Services[service_name]}
+
+
+@app.get("/service/{service_name}/commands")
+def get_service_commands(service_name: str):
+    commands = [
+        component["commands"] for component in Services[service_name]["components"] 
+        if component.get("commands", None) != None
+    ]
+    return {f"Commands for {service_name}": commands}
+
+
+@app.get("/commands")
+def get_commands():
+    return {"Commands": Commands}
+
+
+@app.get("/commands/{command}")
+def get_command(command: str):
+    commands = {cmd["name"]: cmd for cmd in Commands}
+    return {"Command": commands[command]}
+
